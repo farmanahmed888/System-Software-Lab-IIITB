c94eb5b14090d2aa407a15df4fef5b413a24354a
farmanahmed888 <farmanahmed888@gmail.com>
2023-08-11 05:38:30 +0000

    August11

--- Q2/2.c
+++ Q2/2.c
@@ -4,17 +4,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 int main(){
-    int v=symlink("dest", "destSL");
-    if(v<0){
-        perror("Failed");
-        return 1;
+    while(1){
+        
     }
-    int f=link("dest", "destHL");
-    if(f<0){
-        perror("failed");
-        return 1;
-    }
-    int e=mknod("destFIFO", S_IFIFO, 0);
-    if(e<0) perror("Failed");
-    return 0;
 }
