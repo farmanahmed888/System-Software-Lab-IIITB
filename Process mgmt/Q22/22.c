4c662226baae369a3e1dae87887064aa6b44411a
farmanahmed888 <farmanahmed888@gmail.com>
2023-09-09 18:21:57 +0000

    9Sep

--- Process mgmt/Q22/22.c
+++ Process mgmt/Q22/22.c
@@ -6,8 +6,16 @@
 int main(){
     int fd;
     fd=open("input.txt",O_WRONLY|O_APPEND);
-    fork();
-    dprintf(fd,"\n");
-    //write(fd, const void *__buf, size_t __n)
-    dprintf(fd,"hello\n\n");
+    int fort=fork();
+    if(fort==0){
+        char *childwrites="child is writing\n";
+        lseek(fd, sizeof(childwrites), SEEK_CUR);
+        write(fd, childwrites, sizeof(childwrites));
+        printf("child has written\n");
+    }else{
+        char *parentwrite="parent is writing\n";
+        lseek(fd, sizeof(parentwrite), SEEK_CUR);
+        write(fd, parentwrite, sizeof(parentwrite));
+        printf("parent has written\n");
+    }
 }
